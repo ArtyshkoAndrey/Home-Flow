5448245fed481e2460146cd32c20eacaff8c8af1
Fulliton <artyshko.andrey@gmail.com>
2021-06-20 21:46:43 +0700

    Управление переключателями

--- app/Http/Controllers/Api/ModuleController.php
+++ app/Http/Controllers/Api/ModuleController.php
@@ -8,7 +8,9 @@ use App\Models\Room;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use PhpMqtt\Client\ConnectionSettings;
+use PhpMqtt\Client\Exceptions\MqttClientException;
+use PhpMqtt\Client\MqttClient;
 
 class ModuleController extends Controller
 {
@@ -77,8 +79,62 @@ class ModuleController extends Controller
   public function update(Request $request, int $id): JsonResponse
   {
     $module = Module::find($id);
-    $module->update($request->all());
-    $module->save();
+    if ($request->has('data')) {
+      $module->data = $request->get('data');
+      $module->save();
+
+      try {
+        // Create a new instance of an MQTT client and configure it to use the shared broker host and port.
+        $client = new MqttClient(
+          config('mqtt-client.connections.default.host'),
+          (int)config('mqtt-client.connections.default.port'),
+          config('mqtt-client.connections.default.client_id') . "-rand",
+        );
+
+//    Данные авторизации для MQTT
+        $connectionSettings = (new ConnectionSettings())
+          ->setUsername(config('mqtt-client.connections.default.connection_settings.auth.username'))
+          ->setPassword(config('mqtt-client.connections.default.connection_settings.auth.password'));
+
+//    Подключение к MQTT
+        $client->connect(
+          $connectionSettings,
+          false
+        );
+
+        // Publish the message 'Hello world!' on the topic 'foo/bar/baz' using QoS 1.
+        $client->publish($module->mqtt, $request->get('data'), MqttClient::QOS_AT_MOST_ONCE);
+
+        // Since QoS 1 requires the publisher to await confirmation and resend the message if no confirmation is received,
+        // we need to start the client loop which takes care of that. By passing `true` as second parameter,
+        // we allow the loop to exit as soon as all confirmations have been received.
+
+        // Gracefully terminate the connection to the broker.
+        $client->disconnect();
+      } catch (MqttClientException $e) {
+        // MqttClientException is the base exception of all exceptions in the library. Catching it will catch all MQTT related exceptions.
+        return response()->json($e->getMessage(),500);
+      }
+    } else {
+      $request->validate([
+        'name' => 'required|string',
+        'google_index' => 'required|string',
+        'ico' => 'required|string|min:1',
+        'mqtt' => 'required|string',
+        'room' => 'required|exists:rooms,id',
+        'type' => 'required|exists:types,id',
+      ]);
+
+      $data = $request->all();
+      $module->update($data);
+      $module->data = null;
+      $module->room()
+        ->associate($data['room']);
+      $module->type()
+        ->associate($data['type']);
+      $module->save();
+    }
+
     return response()->json(['module' => $module]);
   }
 
--- resources/js/components/HomeModuleInfo.vue
+++ resources/js/components/HomeModuleInfo.vue
@@ -40,8 +40,12 @@
 
             <div v-else-if="module.type.type === 'switch' || module.type.type === 'light'">
               <p class="card-text">
-                <strong v-if="module.data === '1'" class="text-primary">Включён</strong>
-                <strong v-else class="text-danger">Выключён</strong>
+<!--                <strong v-if="module.data === '1'" class="text-primary">Вкл.</strong>-->
+<!--                <strong v-else class="text-danger">Выкл.</strong>-->
+                <label class="switch">
+                  <input v-model="computedSwitchLightDataToBoolean" type="checkbox">
+                  <span class="slider round" />
+                </label>
                 <br>
                 <small class="text-muted">{{ module.updated_date }}</small>
               </p>
@@ -54,6 +58,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HomeModuleInfo',
   props: {
@@ -65,11 +71,47 @@ export default {
   computed: {
     icoType: function () {
       if (this.module.type.type === 'temperature') {
-        return 'C'
-      } else if (this.module.type.name === 'humidity') {
+        return '°C'
+      } else if (this.module.type.type === 'humidity') {
         return '%'
       }
-      return ''
+      return 'Ошибка'
+    },
+    computedSwitchLightDataToBoolean: {
+      get: function () {
+        return this.module.data === '1'
+      },
+      set: function (newValue) {
+        // eslint-disable-next-line vue/no-mutating-props
+        this.module.data = newValue ? '1' : '0'
+        console.log(newValue)
+        axios.put('/api/module/' + this.module.id, {
+          data: newValue ? '1' : '0'
+        })
+          .then(response => {
+            console.log(response.data)
+            // eslint-disable-next-line vue/no-mutating-props
+            this.module.data = response.data.module.data
+          })
+          .catch(error => {
+            console.log(error.response.data)
+          })
+      }
+    }
+  },
+  methods: {
+    onChangeEventHandlerSwitchLightBool (newValue) {
+      axios.put('/api/module/' + this.module.id, {
+        data: newValue ? '1' : '0'
+      })
+        .then(response => {
+          console.log(response.data)
+          // eslint-disable-next-line vue/no-mutating-props
+          this.module.data = response.data.module.data
+        })
+        .catch(error => {
+          console.log(error.response.data)
+        })
     }
   }
 }
@@ -104,5 +146,67 @@ export default {
   display: block;
   /*content: this.module.ico;*/
   font-family: 'Font Awesome 5 Free', serif
+}
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
